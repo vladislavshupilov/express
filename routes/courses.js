95de20b3d76c67f656823b342d94cc57dd324a41
pavelkinksepp <pavel.kinksepp@ivkhk.ee>
2023-05-18 12:24:41 +0300

    express

---
+++ routes/courses.js
@@ -0,0 +1,14 @@
+const {Router}=require('express')
+const Course = require('../models/course')
+const router=Router()
+
+router.get('/', async (req,res)=>{
+    const courses = await Course.getAll()
+    res.render('courses', {
+        title: 'Courses',
+        isCourses: true,
+        courses
+    })
+})
+
+module.exports = router;
